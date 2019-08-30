e828da95389054182695b571e6fdfdf4e43241a5
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2019-08-30 05:11:29 +0200

    Still working on message slider. It's quite bugged atm...

--- src/app/components/buttons/slide/message.js
+++ src/app/components/buttons/slide/message.js
@@ -18,26 +18,36 @@ function SlideToMessageButton({ window: { width: hw, height: vh } }) {
   const containerRef = useRef();
   const buttonRef = useRef();
   const [imageStyle, setImageStyle] = useState();
+  const [initialImageBounds, setInitialImageBounds] = useState(null);
   return (
     <div ref={containerRef} className="slide-to-message-button-container">
       <img
         style={{ ...imageStyle }}
         ref={buttonRef}
         onDrag={e => {
+          const { left, right, top, bottom } = ReactDOM.findDOMNode(
+            buttonRef.current
+          ).getBoundingClientRect();
+          if (!initialImageBounds) {
+            setInitialImageBounds({ left, right, top, bottom });
+          }
           const mouse = { x: e.pageX, y: e.pageY };
           const { left: max, right: min } = ReactDOM.findDOMNode(
             containerRef.current
           ).getBoundingClientRect();
-          const { left: buttonLeft } = ReactDOM.findDOMNode(
-            buttonRef.current
-          ).getBoundingClientRect();
           const halfImageWidth = vh <= hw ? vh * 0.07 : hw * 0.07;
           const difference = min - mouse.x;
-          let marginLeft = min - difference - max - halfImageWidth - 3;
+          let marginLeft = min - difference - max - halfImageWidth - 2;
           let paddingRight = min - (mouse.x + halfImageWidth);
           paddingRight =
             marginLeft < 0 ? paddingRight + -marginLeft : paddingRight;
-          setImageStyle({ marginLeft, paddingRight });
+          if (
+            initialImageBounds &&
+            mouse.x - halfImageWidth < initialImageBounds.left &&
+            mouse.x - halfImageWidth + 3 > max
+          ) {
+            setImageStyle({ marginLeft, paddingRight });
+          }
         }}
         onDragEnd={() => {
           setImageStyle(null);
