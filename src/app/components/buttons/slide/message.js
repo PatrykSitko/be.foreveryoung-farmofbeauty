7386f5ad2e83e607761ce8698ada04fe1a8b4966
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2019-09-01 12:32:27 +0200

    Made the button work properly on mobile

--- src/app/components/buttons/slide/message.js
+++ src/app/components/buttons/slide/message.js
@@ -14,10 +14,22 @@ const mapStateToProps = ({
   return { window: { width, height } };
 };
 
-function handleDragEvent(containerRef, hw, vh, setImageStyle, e) {
+function handleDragEvent(
+  containerRef,
+  hw,
+  vh,
+  setImageStyle,
+  allowedToTriggerCallEvent,
+  setAllowedToTriggerCallEvent,
+  event
+) {
   const mouse = {
-    x: e.pageX || (e.nativeEvent.touches && e.nativeEvent.touches[0].pageX),
-    y: e.pageY || (e.nativeEvent.touches && e.nativeEvent.touches[0].pageY)
+    x:
+      event.pageX ||
+      (event.nativeEvent.touches && event.nativeEvent.touches[0].pageX),
+    y:
+      event.pageY ||
+      (event.nativeEvent.touches && event.nativeEvent.touches[0].pageY)
   };
   const { left: max, right: min } = ReactDOM.findDOMNode(
     containerRef.current
@@ -28,36 +40,60 @@ function handleDragEvent(containerRef, hw, vh, setImageStyle, e) {
   let marginLeft = min - difference - max - halfImageWidth - border;
   let paddingRight = min - (mouse.x + halfImageWidth);
   paddingRight = marginLeft < 0 ? paddingRight + -marginLeft : paddingRight;
+  if (!allowedToTriggerCallEvent && marginLeft <= 0) {
+    setAllowedToTriggerCallEvent(true);
+  }
   setImageStyle(
     mouse.x + halfImageWidth < min + border &&
       mouse.x !== 0 && {
         marginLeft: marginLeft > 0 ? marginLeft : 0,
-        paddingRight
+        paddingRight: marginLeft - border < 0 ? paddingRight - border * 2 : 0
       }
   );
 }
+
 function SlideToMessageButton({ window: { width: hw, height: vh } }) {
   const containerRef = useRef();
   const buttonRef = useRef();
   const [imageStyle, setImageStyle] = useState();
+  const [allowedToTriggerCallEvent, setAllowedToTriggerCallEvent] = useState(
+    false
+  );
 
   return (
     <div ref={containerRef} className="slide-to-message-button-container">
       <img
         style={{ ...imageStyle }}
         ref={buttonRef}
-        onDrag={handleDragEvent.bind(this, containerRef, hw, vh, setImageStyle)}
+        onDrag={handleDragEvent.bind(
+          this,
+          containerRef,
+          hw,
+          vh,
+          setImageStyle,
+          allowedToTriggerCallEvent,
+          setAllowedToTriggerCallEvent
+        )}
         onDragEnd={() => {
-          setImageStyle(null);
+          if (allowedToTriggerCallEvent) {
+            setAllowedToTriggerCallEvent(!allowedToTriggerCallEvent);
+            alert("hello");
+          }
         }}
         onTouchMove={handleDragEvent.bind(
           this,
           containerRef,
           hw,
           vh,
-          setImageStyle
+          setImageStyle,
+          allowedToTriggerCallEvent,
+          setAllowedToTriggerCallEvent
         )}
         onTouchEnd={() => {
+          if (allowedToTriggerCallEvent) {
+            setAllowedToTriggerCallEvent(!allowedToTriggerCallEvent);
+            alert("hello");
+          }
           setImageStyle(null);
         }}
         className="slide-to-message-button"
